661f71f7bfd1333fb0cbb5d1cc064fbd7755fe8b
akrush <akrush24@gmail.com>
2020-02-23 13:19:40 +0300

    edit index.php

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 echo "Today is " . date("Y/m/d") . "<br>";
 
 echo gethostname(); // может вывести например: sandie
-
+echo '<br>'
 echo gethostbyaddr($_SERVER['REMOTE_ADDR']);
+echo '<br>'
+
 
 # phpinfo()
 
