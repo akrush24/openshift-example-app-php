f21873c7f888fc438eb759e2faaabb286502df00
akrush <akrush24@gmail.com>
2020-02-23 12:41:25 +0300

    edit index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+
 phpinfo()
 
 ?>
