fcc55af45727ae7c164fdcada0715f277603e574
Andrey <akrush24@gmail.com>
2021-07-31 22:50:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ echo "<tr><td>Today is</td><td><b>" . date("Y/m/d") . "</b></td></tr>";
 
 echo "<tr><td>Node hostname</td><td><b>" . gethostname() . '</b></td></tr>'; // может вывести например: sandie
 echo "<tr><td>Node IP</td><td><b>" . gethostbyaddr($_SERVER['REMOTE_ADDR']) . '</b></td></tr>';
-
-
-# phpinfo()
+echo '<tr><td>PHP Version: ' . phpversion(). '</b></td></tr>';
 
 ?>
